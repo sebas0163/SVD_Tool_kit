c86bbdd0f6768277e3a709c443207177d06a6c57
Sebas <sebas0163@estudiantec.cr>
2025-03-03 22:33:06 -0600

    [Add] GSVD with bugs

---
+++ GSVD.py
@@ -0,0 +1,38 @@
+import numpy as np
+import time as tm
+from scipy.linalg import cossin
+
+#A and B must have the same number of columns
+def GSVD(matrix_A,matrix_B):
+    m,n = matrix_A.shape
+    j,k = matrix_B.shape
+    if k != n: #Validate the number of columns
+        print("Number of columns between matrix A and B are different")
+    else:
+        stacked_matrix = np.vstack((matrix_A,matrix_B))
+        rank = np.linalg.matrix_rank(stacked_matrix)
+        q, s,z =np.linalg.svd(stacked_matrix) #Calculates the SVD vals (change for my implementation complete)
+        sigma_r = np.diag(s[:rank])#Here works well
+        u,cs,vh = cossin(q, p=m+j-1, q=rank-1) #Here makes de CS decomposition for matrix Q obtained in the svd
+        u_1 = u[:m,:m] #Calculates U1 from u
+        u_2 =u[m:,m:] #Calculates U2 from u
+        d_a = cs[:rank, :rank] #Takes de d_a matrix from CS matrix
+        d_b = cs[rank:,:rank] #Takes de d_a matrix from CS matrix tiene un bug
+        v_1 = vh[:m,:m] @ sigma_r #Calculates v1 from vh matrix and multiplies by sigma 
+        im = np.eye(n-rank) #Generates a Identity matrix with dimentions n-rank x n-rank
+        zero_block_1 = np.zeros((rank,n-rank)) #Create a zero block to complete the new matrix
+        zero_block_2 = np.zeros((n-rank, rank))#Create a zero block to complete the new matrix
+        top_block = np.hstack((v_1,zero_block_1))
+        bottom_block = np.hstack((zero_block_2,im))
+        combined_matrix = np.vstack((top_block,bottom_block))#Creates the new coombined matrix
+        x = z.T @ np.linalg.inv(combined_matrix) #Calculates x doing Z dot (combined_matrix)^-1
+        A_exp = u_1 @ d_a @ np.linalg.inv(x) 
+        B_exp = u_2@d_b @ np.linalg.inv(x)
+        print("A exp \n", A_exp)
+        print("Original \n", matrix_A)
+        print("B exp \n", B_exp)
+        print("Original \n",matrix_B)
+        #Continuar con calculo de Da y Db junto con cs decomposition
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+b = np.array([[4,2,3],[8,5,6],[10,12,14]])
+GSVD(a,b)
